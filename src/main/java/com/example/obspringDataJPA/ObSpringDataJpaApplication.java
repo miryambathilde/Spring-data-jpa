6d95055c71fd6635e7e193e8408309900b74b35a
Miryam <mbathilde@gmail.com>
2021-10-16 20:57:11 +0200

    commit initial - proyecto creado con Spring Boot + Spring Data JPA + H2

---
+++ src/main/java/com/example/obspringDataJPA/ObSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.obspringDataJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObSpringDataJpaApplication.class, args);
+	}
+
+}
