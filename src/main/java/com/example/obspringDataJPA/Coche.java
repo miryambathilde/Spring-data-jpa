02cee618e7f47ed65f5bf393a1c7bdd80f4fdb31
Miryam <mbathilde@gmail.com>
2021-10-16 21:12:30 +0200

    Creamos clase Coche para nuestra BBDD

---
+++ src/main/java/com/example/obspringDataJPA/Coche.java
@@ -0,0 +1,68 @@
+package com.example.obspringDataJPA;
+
+public class Coche {
+
+    // 1. Atributos
+    // atributos encapsulados por private
+    private Long id; //es nuestra primary key de la db
+    private String manufacturer;
+    private String model;
+    private Integer year;
+
+    // 2. constructores
+
+    public Coche() {}
+
+    public Coche(Long id, String manufacturer, String model, Integer year) {
+        this.id = id;
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.year = year;
+    }
+
+    // 3. getter y setter
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    // to string con concat
+
+    @Override
+    public String toString() {
+        return "Coche{" +
+                "id=" + id +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
